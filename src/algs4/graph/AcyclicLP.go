8d88797ee07c2a1e37673668a496fef04f780e96
yinyajun-hj <yinyajun-hj@huajiao.tv>
2020-01-07 23:35:20 +0800

    modifies

--- src/algs4/graph/AcyclicLP.go
+++ src/algs4/graph/AcyclicLP.go
@@ -1,32 +1,34 @@
 package graph
 
 import (
-	"algs4/stack"
 	"util"
+	"algs4/stack"
 )
 
 /**
-*  represents a data type for solving the single-source shortest paths problem in edge-weighted
+*  represents a data type for solving the single-source longest paths problem in edge-weighted
 *  directed acyclic graphs (DAGs). The edge weights can be positive, negative, or zero.
 *
 * @see
 * @author Golang translation by Yajun Yin from Java by Robert Sedgewick and Kevin Wayne.
- */
+*/
 
-type AcyclicSP struct {
+type AcyclicLP struct {
 	distTo []float64
 	edgeTo []*DirectedEdge
 }
 
-func NewAcyclicSP(g *EdgeWeightedDigraph, s int) *AcyclicSP {
-	m := &AcyclicSP{}
+const NEGATIVE_INFINITY = -POSTIVE_INFINITY
+
+func NewAcyclicLP(g *EdgeWeightedDigraph, s int) *AcyclicLP {
+	m := &AcyclicLP{}
 	m.distTo = make([]float64, g.V())
 	m.edgeTo = make([]*DirectedEdge, g.V())
 
 	m.validateVertex(s)
 
 	for v := 0; v < g.V(); v++ {
-		m.distTo[v] = POSTIVE_INFINITY
+		m.distTo[v] = NEGATIVE_INFINITY
 	}
 	m.distTo[s] = 0.0
 
@@ -45,26 +47,26 @@ func NewAcyclicSP(g *EdgeWeightedDigraph, s int) *AcyclicSP {
 	return m
 }
 
-func (m *AcyclicSP) relax(e *DirectedEdge) {
+func (m *AcyclicLP) relax(e *DirectedEdge) {
 	v := e.From()
 	w := e.To()
-	if m.distTo[w] > m.distTo[v]+e.Weight() {
+	if m.distTo[w] < m.distTo[v]+e.Weight() {
 		m.distTo[w] = m.distTo[v] + e.Weight()
 		m.edgeTo[w] = e
 	}
 }
 
-func (m *AcyclicSP) DistTo(v int) float64 {
+func (m *AcyclicLP) DistTo(v int) float64 {
 	m.validateVertex(v)
 	return m.distTo[v]
 }
 
-func (m *AcyclicSP) HasPathTo(v int) bool {
+func (m *AcyclicLP) HasPathTo(v int) bool {
 	m.validateVertex(v)
 	return m.distTo[v] < POSTIVE_INFINITY
 }
 
-func (m *AcyclicSP) PathTo(v int) util.Iterator {
+func (m *AcyclicLP) PathTo(v int) util.Iterator {
 	m.validateVertex(v)
 	path := stack.NewStack()
 	for e := m.edgeTo[v]; e != nil; e = m.edgeTo[e.From()] {
@@ -74,7 +76,7 @@ func (m *AcyclicSP) PathTo(v int) util.Iterator {
 
 }
 
-func (m *AcyclicSP) validateVertex(v int) {
+func (m *AcyclicLP) validateVertex(v int) {
 	V := len(m.distTo)
 	if v < 0 || v >= V {
 		panic("invalid vertex")
---
+++ src/test/acyclicLP.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"algs4/graph"
+	. "util"
+)
+
+/**
+* $ go run src/test/acyclicLP.go 5 < data/tinyEWDAG.txt
+* 5 to 0 (2.44)  5 -> 1  0.32   1 -> 3  0.29   3 -> 6  0.52   6 -> 4  0.93   4 -> 0  0.38
+* 5 to 1 (0.32)  5 -> 1  0.32
+* 5 to 2 (2.77)  5 -> 1  0.32   1 -> 3  0.29   3 -> 6  0.52   6 -> 4  0.93   4 -> 7  0.37   7 -> 2  0.34
+* 5 to 3 (0.61)  5 -> 1  0.32   1 -> 3  0.29
+* 5 to 4 (2.06)  5 -> 1  0.32   1 -> 3  0.29   3 -> 6  0.52   6 -> 4  0.93
+* 5 to 5 (0.00)
+* 5 to 6 (1.13)  5 -> 1  0.32   1 -> 3  0.29   3 -> 6  0.52
+* 5 to 7 (2.43)  5 -> 1  0.32   1 -> 3  0.29   3 -> 6  0.52   6 -> 4  0.93   4 -> 7  0.37
+*
+* @see
+* @author Golang translation by Yajun Yin from Java by Robert Sedgewick and Kevin Wayne.
+*/
+
+func main() {
+	in := NewIn(os.Stdin)
+	g := graph.NewEdgeWeightedDigraphWithIn(in)
+	s, _ := strconv.Atoi(os.Args[1])
+
+	// find shortest path from s to each other vertex in DAG
+	sp := graph.NewAcyclicLP(g, s)
+	for v := 0; v < g.V(); v ++ {
+		if sp.HasPathTo(v) {
+			fmt.Printf("%d to %d (%.2f)  ", s, v, sp.DistTo(v))
+			edges := sp.PathTo(v)
+			for e := edges.Next(); e != nil; e = edges.Next() {
+				fmt.Print(e, "   ")
+			}
+			fmt.Println()
+		} else {
+			fmt.Printf("%d to %d         no path\n", s, v)
+		}
+	}
+}
