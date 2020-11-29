5314e0d4abf863940aa0de0cca6bb6353a84fd27
yinyajun-hj <yinyajun-hj@huajiao.tv>
2020-11-30 00:14:46 +0800

    merge sort

--- impl/sorting/examples/sort_compare.go
+++ impl/sorting/examples/sort_compare.go
@@ -32,6 +32,8 @@ func (c *SortCompare) time(alg string, a []float64) time.Duration {
 		sorter = sorting.NewShell()
 	case "Merge":
 		sorter = sorting.NewMerge()
+	case "MergeBU":
+		sorter = sorting.NewMergeBU()
 	case "AdvancedMerge":
 		sorter = sorting.NewAdvancedMerge()
 	default:
@@ -59,7 +61,7 @@ func main() {
 	t := new(SortCompare)
 	N := 500000
 	T := 10
-	for _, alg := range []string{"Merge", "AdvancedMerge"} {
+	for _, alg := range []string{"Merge", "AdvancedMerge", "MergeBU"} {
 		consume := t.timeRandomInput(alg, N, T)
 		utils.StdOut.Println(alg, consume)
 	}
